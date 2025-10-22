6adf8491c659cf3faf13c3dc609d3b6c1aefa15c
Marianna <marianna@syndeo.cx>
2025-10-23 02:50:29 +0300

    Solution

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
-import playwright from 'eslint-plugin-playwright';
+import playwright from "eslint-plugin-playwright";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    ...js.configs.recommended, 
-    languageOptions: { 
+  js.configs.recommended,
+  playwright.configs['flat/recommended'],
+  {
+    languageOptions: {
       globals: {
         console: "readonly",
         process: "readonly",
@@ -20,22 +20,18 @@ export default defineConfig([
         setTimeout: "readonly",
         setInterval: "readonly",
         clearTimeout: "readonly",
-        clearInterval: "readonly"
-      }
+        clearInterval: "readonly",
+      },
     },
-  },
-  { 
-    ...js.configs.recommended,
-    ...playwright.configs['flat/recommended'],
     rules: {
-      'no-unused-vars': 'error',
-      'max-len': ['error', { code: 80, comments: 80 }],
-      'playwright/expect-expect': 'off',   
+      "no-unused-vars": "error",
+      "max-len": ["error", { code: 80, comments: 80 }],
+      "playwright/expect-expect": "off",
     },
     ignores: [
-      '**/node_modules/*',
-      'playwright.config.js',
-      '**/playwright-report/**',
+      "**/node_modules/*",
+      "playwright.config.js",
+      "**/playwright-report/**",
     ],
   },
 ]);
